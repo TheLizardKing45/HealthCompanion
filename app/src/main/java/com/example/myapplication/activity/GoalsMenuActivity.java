1ba6c48d44b8ed77aecadf81df2bdc4ae4a0d417
Brett Capistrano <bretlourd@gmail.com>
2021-01-10 10:07:00 -0800

    added add button without functionality

--- app/src/main/java/com/example/myapplication/activity/GoalsMenuActivity.java
+++ app/src/main/java/com/example/myapplication/activity/GoalsMenuActivity.java
@@ -1,18 +1,20 @@
 package com.example.myapplication.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 import com.example.myapplication.R;
 import com.example.myapplication.activity.body.BodyMenuActivity;
@@ -25,16 +27,19 @@ public class GoalsMenuActivity extends AppCompatActivity {
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView todoItems;
+    private Toolbar toolbar;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        todoItems = (ListView) findViewById(R.id.todoItems);
-        items = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
-        todoItems.setAdapter(itemsAdapter);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Health Companion");
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().hide();
+        configureTodoList();
 
         items.add("test 1");
         items.add("test 2");
@@ -45,34 +50,51 @@ public class GoalsMenuActivity extends AppCompatActivity {
         configureListViewListener();
         configureBodyButton();
         configureMentalButton();
+        configureAddButton();
+    }
+
+    private void configureTodoList() {
+        todoItems = (ListView) findViewById(R.id.todoItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        todoItems.setAdapter(itemsAdapter);
+    }
+
+    private void configureAddButton() {
+
     }
 
     private void configureListViewListener() {
         todoItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(GoalsMenuActivity.this);
-                alert.setTitle("Delete task?");
-                alert.setMessage("Are you sure you want to delete this task?");
-                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        System.out.println("test");
-                        items.remove(position);
-                        dialog.dismiss();
-                        itemsAdapter.notifyDataSetChanged();
-                    }
-                });
-                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                alert.show();
+                createConfirmDeleteDialog(position);
                 return true;
             }
         });
+
+    }
+
+    private void createConfirmDeleteDialog(int position) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(GoalsMenuActivity.this);
+        alert.setTitle("Delete task?");
+        alert.setMessage("Are you sure you want to delete this task?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("test");
+                items.remove(position);
+                dialog.dismiss();
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.show();
     }
 
 
